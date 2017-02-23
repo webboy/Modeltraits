0ee5bc9555bd6278520ba024fbceeeeee6260259
webboy <extramedia.nemanja@gmail.com>
2017-02-23 23:13:13 +0100

    Update SortableModelTrait

--- src/SortableModelTrait.php
+++ src/SortableModelTrait.php
@@ -3,10 +3,14 @@ namespace Webboy\Modeltraits;
 
 trait SortableModelTrait
 {
+	protected $default_order_by = 'id';
+
+	protected $default_order_type = 'asc';
+
 	public function scopeSort($query, $order_by = null, $order_type = null)
     {
-        if(empty($order_by)) $order_by = 'id';
-        if(empty($order_type)) $order_type = 'asc';
+        if(empty($order_by)) $order_by = $this->default_order_by;
+        if(empty($order_type)) $order_type = $this->default_order_type;
 
         return $query->orderBy($order_by,$order_type);
 
