72601f37dbc33a37817d965fddfe86eb479faa1e
webboy <extramedia.nemanja@gmail.com>
2017-10-05 13:31:46 +0200

    1.2.1.1

--- src/SortableModelTrait.php
+++ src/SortableModelTrait.php
@@ -11,8 +11,8 @@ trait SortableModelTrait
 
 	public function scopeSort(Builder $query, $order_by = null, $order_type = null)
     {
-        if(empty($order_by)) $order_by = $this->default_order_by;
-        if(empty($order_type)) $order_type = $this->default_order_type;
+        if(empty($order_by)) $order_by = self::$default_order_by;
+        if(empty($order_type)) $order_type = self::$default_order_type;
 
         return $query->orderBy($this->table.'.'.$order_by,$order_type);
 
